4057780a778e7f40a11a8927d35bbe8424d21954
Max Matkovski <maxmatkovski@gmail.com>
2026-08-09 18:13:17 +0300

    Refine live world clock

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function updateWorldClock() {
           timeZone,
           hour: "2-digit",
           minute: "2-digit",
-          second: "2-digit",
           hour12: false,
         }),
       );
